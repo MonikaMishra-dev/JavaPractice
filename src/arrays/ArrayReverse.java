7da60c82450680464dca655b5f00014f1bc1cbbb
Monika Jha <monika.mishra2497@gmail.com>
2026-01-09 22:59:54 +0530

    Reversing an array

---
+++ src/arrays/ArrayReverse.java
@@ -0,0 +1,16 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ArrayReverse {
+    public static void main(String[] args) {
+        int[] arr = {40,35,23,46,7,90};
+        int temp;
+        for(int i=0;i<arr.length/2;i++){
+            temp = arr[i];
+            arr[i] = arr[arr.length - i-1];
+            arr[arr.length -i-1] = temp;
+        }
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+}

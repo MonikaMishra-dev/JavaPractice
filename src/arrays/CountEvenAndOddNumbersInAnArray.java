a5d83205e79e8cb14ec576fec40c778ad0390a4a
Monika Jha <monika.mishra2497@gmail.com>
2026-01-13 13:10:13 +0530

    Count even and odd numbers in an array

---
+++ src/arrays/CountEvenAndOddNumbersInAnArray.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class CountEvenAndOddNumbersInAnArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        int even=0,odd=0;
+        for(int num : arr){
+            if(num%2==0)
+                even++;
+            else odd++;
+        }
+        System.out.println("Even count = "+even);
+        System.out.println("Odd count = "+odd);
+    }
+}

b285d7b3147771a35d2bd6150d277c62c4f5caa8
Monika Jha <monika.mishra2497@gmail.com>
2026-01-13 12:54:10 +0530

    Copy one array into another

---
+++ src/arrays/CopyOneArrayIntoAnother.java
@@ -0,0 +1,22 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class CopyOneArrayIntoAnother {
+    public static void main(String[] args) {
+        int[] arr = {1,4,2,6,7,3};
+        int[] arr1 = new int[arr.length];
+//        for(int i=0;i<arr.length;i++){
+//            arr1[i] = arr[i];
+//        }
+
+
+        System.arraycopy(arr,0,arr1,0,arr.length);
+
+
+//       arr1 =  Arrays.copyOf(arr,arr.length);
+        for(int num : arr1){
+            System.out.print(num+" ");
+        }
+    }
+}
